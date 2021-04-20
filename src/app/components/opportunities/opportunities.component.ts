c2ee13767b2d35c35d8aa690178fd71a70b68b35
Dhruv Miglani <dhruvmiglani.23@gmail.com>
2021-04-20 10:41:23 +0530

    minor tweaks

--- src/app/components/opportunities/opportunities.component.ts
+++ src/app/components/opportunities/opportunities.component.ts
@@ -3,6 +3,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 import {Opportunity} from '../../models/opportunity.model';
 import { OpportunityService } from 'src/app/services/opportunity.service';
 import { FormBuilder } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-opportunities',
   templateUrl: './opportunities.component.html',
@@ -18,19 +19,22 @@ import { FormBuilder } from '@angular/forms';
 export class OpportunitiesComponent implements OnInit {
   // dataSource = ELEMENT_DATA;
   dataSource?:any;
-  columnsToDisplay = ['id', 'skills', 'date', 'location'];
+  dataS?:any;
+  columnsToDisplay = ['id', 'skills', 'date', 'location','  '];
   expandedElement?: Opportunity | null;
-  data?:Opportunity[];
   opportunityForm = this.fb.group({
     ed:[''],
     skills:[''],
     location:[''],
     date:['']
   })
+   displayedColumns:string[] = ['position', 'name', 'weight', 'symbol'];
   constructor(private opportunityService:OpportunityService,private fb: FormBuilder) { }
 
   ngOnInit(): void {
      this.getEmployee();
+     
+    this.dataS = new MatTableDataSource(this.dataSource);
   }
 
   openDialog(opportunity:any,mode:string){
@@ -67,5 +71,10 @@ export class OpportunitiesComponent implements OnInit {
       console.log("error to add opprotunity")
     });
   }
+  
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataS.filter = filterValue.trim().toLowerCase();
+  }
 }
--- src/app/services/opportunity.service.ts
+++ src/app/services/opportunity.service.ts
@@ -11,13 +11,17 @@ export class OpportunityService {
   constructor(private http:HttpClient) { }
 
   public getEmployees(): Observable<Opportunity[]> {
-    return this.http.get<Opportunity[]>(`http://localhost:8080/opportunity/all`);
+    return this.http.get<Opportunity[]>(`http://localhost:8080/opportunity`);
   }
 
   public addEmployee(opportunity: Opportunity): Observable<Opportunity> {
     console.log("in service ",opportunity)
-    return this.http.post<Opportunity>(`http://localhost:8080/opportunity/add`, opportunity);
+    return this.http.post<Opportunity>(`http://localhost:8080/opportunities/add`, opportunity);
 
+  }
+
+  public deleteOpportunity(id:number){
+    return this.http.delete(`http://localhost:8080/opportunity/delete/${id}`);
   }
 
 }
